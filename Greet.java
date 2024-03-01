806ddd16221b3c5f5eb2c2e6dc3c3a21c31075f3
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-03-01 12:57:43 +0000

    third commit

--- Greet.java
+++ Greet.java
@@ -2,7 +2,8 @@
 
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("How are you?");  // ADDED
+        String greeting = "Good morning";  // ADDED
+        System.out.println(greeting);  // MODIFIED
+        System.out.println("How are you?"); 
     }
 }
